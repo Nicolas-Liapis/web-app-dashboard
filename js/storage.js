39b19d9fd67b7aefa36a825ac7711acc1e3d6ea4
Nicolas <nicolas.liapis.dev@gmail.com>
2017-07-04 13:01:36 +0100

    v0.9
    fixed minor CSS issues. Local storage not ready yet.

---
+++ js/storage.js
@@ -0,0 +1,44 @@
+
+
+var save_button = document.getElementById('save');
+
+save_button.addEventListener('click', function() {
+  // checkboxes
+  var switch1 = document.getElementById('checkbox1');
+  localStorage.setItem('switch1', switch1.checked);
+
+  var switch2 = document.getElementById('checkbox2');
+  localStorage.setItem('switch2', switch2.checked);
+
+  console.log(localStorage.switch1);
+  // timezone
+  var timezone = document.getElementById('time');
+  localStorage.setItem('timezone', time.value);
+});
+
+function load() {
+  // checkboxes
+// var check1 = localStorage.getItem('switch1');
+ var check1 = localStorage.getItem('switch1');
+ console.log(check1);
+
+
+
+
+
+
+  // timezone
+  var timezone = localStorage.getItem('timezone');
+  if (timezone) {
+    document.getElementById('time').value = timezone;
+  }
+};
+
+var cancel_button = document.getElementById('cancel');
+
+cancel_button.addEventListener('click', function() {
+  location.reload();
+  localStorage.clear();
+});
+
+load();
